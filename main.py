7a063ed7456f26fe1ffee542ea1c413e3bec2ed1
madewulf <mdewulf@bluesquarehub.com>
2023-04-30 16:38:34 +0200

    More data on areas

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ from datetime import date
 import locale
 from staticmap import StaticMap, CircleMarker, StaticMap, Line, Polygon
 from haversine import haversine
+from jinja2 import Undefined, Template
+
+
 server = "https://iaso.bluesquare.org"
 forms_endpoint = server + "/api/forms/"
 instances_endpoint = server + "/api/instances/"
+
+
+class SilentUndefined(Undefined):
+    def _fail_with_undefined_error(self, *args, **kwargs):
+        return ''
 
 
 def get_headers():
@@ -67,12 +75,36 @@ def get_as_info(as_id):
         headers=headers,
     )
     data["fosas"] = r.json()["orgunits"]
+    as_dict = {}
+    for i in data["fosas"]:
+        file_content =  i.get("file_content")
+        print(file_content)
+        as_dict[i.get("id")] = i
+    print("as_dict", as_dict)
+
     r = requests.get(
         "https://iaso.bluesquare.org/api/orgunits/?validation_status=VALID&orgUnitParentId=%d&onlyDirectChildren=true&limit=1000&order=name&page=1&orgUnitTypeId=211"
         % as_id,
         headers=headers,
     )
     data["localites"] = r.json()["orgunits"]
+    r = requests.get(
+        "https://iaso.bluesquare.org/api/instances/?orgUnitTypeId=207&form_ids=735&limit=200&order=-updated_at&page=1&showDeleted=false&orgUnitParentId=%d"
+        % as_id,
+        headers=headers,
+    )
+
+    fosa_leader_instance = None
+    for i in r.json()["instances"]:
+        file_content =  i.get("file_content")
+        area_dict = as_dict[i.get("org_unit").get("id")]
+        area_dict["donnees_fosa"] = file_content
+        if file_content.get("leader"):
+            fosa_leader_instance = i
+
+    data["donnes_fosa"] = as_dict
+
+    data["fosa_leader_instance"] = fosa_leader_instance
     return data
 
 
@@ -117,9 +149,8 @@ def write_html(data):
     area = data.get("as")
     as_id = area.get("id")
     # Load the Jinja2 template from a separate file
-    env = Environment(loader=FileSystemLoader("."))
+    env = Environment(loader=FileSystemLoader("."), undefined=SilentUndefined)
     template = env.get_template("as_template.html")
-
     # Define the path to the image file
     image_path = "images/%d.png" % as_id
 
@@ -136,13 +167,13 @@ def write_html(data):
     # latitude_district, longitude_district = district.get("latitude"), district.get("longitude")
     # distance_district = haversine(latitude_area, longitude_area, latitude_district, longitude_district)
     # print("distance_district", distance_district)
-    
+
     html = template.render(
         image_path=image_path,
         date_str=date_str,
         data=data,
         count_localite=count_localite,
-        count_fosa=count_fosa,
+        count_fosa=count_fosa
     )
 
     # Write the rendered HTML to a file
@@ -153,11 +184,12 @@ def write_html(data):
 
 
 if __name__ == "__main__":
-    as_ids = [1049730, 1051335, 1050055, 1053714]
+    as_ids = [1056335, 1056978, 1049730, 1051335, 1050055, 1053714]
     for as_id in as_ids:
         data = get_as_info(as_id)
         create_map(data)
         path = write_html(data)
         HTML(path).write_pdf("generated/%d.pdf" % as_id)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
